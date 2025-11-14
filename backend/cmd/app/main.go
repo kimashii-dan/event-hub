74cd5a63dbba0e5d01c37622c2dfdf954f758bfc
Fixsbreaker <alinurkunbol@gmail.com>
2025-11-14 23:12:27 +0500

    add event handler

--- backend/cmd/app/main.go
+++ backend/cmd/app/main.go
@@ -5,36 +5,64 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/Fixsbreaker/event-hub/backend/internal/config"
+	"github.com/Fixsbreaker/event-hub/backend/internal/db"
+	"github.com/Fixsbreaker/event-hub/backend/internal/handler"
+	"github.com/Fixsbreaker/event-hub/backend/internal/middleware"
+	"github.com/Fixsbreaker/event-hub/backend/internal/repository"
+	"github.com/Fixsbreaker/event-hub/backend/internal/service"
+
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
-	"github.com/Fixsbreaker/event-hub/backend/internal/db"
 )
 
 func main() {
-
 	if os.Getenv("ENV") != "production" {
 		if err := godotenv.Load("docker/.env"); err != nil {
 			log.Println("No .env file found, using system environment variables")
 		}
 	}
 
+	// конфиг
+	cfg := config.Load()
+
+	// подключение к БД + миграции
 	db.Connect()
 
-	router := gin.Default()
+	r := gin.Default()
 
-	router.GET("/", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
+	// глобальный логгер запросов
+	r.Use(middleware.Logger())
+
+	// простой health-check
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
 			"message": "EventHub initialized!",
 		})
 	})
 
-	port := os.Getenv("APP_PORT")
+	// auth
+
+	userRepo := repository.NewUserRepository(db.DB)
+	authService := service.NewAuthService(userRepo, cfg.JWTSecret, cfg.JWTExpirationTime)
+	handler.NewAuthHandler(r, authService)
+
+	// events
+
+	eventRepo := repository.NewEventRepository(db.DB)
+	eventService := service.NewEventService(eventRepo)
+	authMW := middleware.Auth(cfg.JWTSecret)
+
+	handler.NewEventHandler(r, eventService, authMW)
+
+	// start server
+
+	port := cfg.ServerPort
 	if port == "" {
 		port = "8080"
 	}
 
-	if err := router.Run(":" + port); err != nil {
+	if err := r.Run(":" + port); err != nil {
 		log.Fatalf("failed to start server: %v", err)
 	}
-
 }
